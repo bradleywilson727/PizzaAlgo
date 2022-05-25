44f1e58301bee77e10053235fd66123d2c5e8484
Bradley Wilson <bradleywilson727@gmail.com>
2022-05-25 12:01:00 -0500

    added order steps

---
+++ Algo/Untitled-1.cs
@@ -0,0 +1,8 @@
+1. Decide which pizza chain to order from.
+2. Go to website.
+3. Decide what toppings you want.
+4. Place your order.
+5. Pay for your order.
+6. Wait for your delivery.
+7. Accept your delivery.
+8. Enjoy your pizza.
