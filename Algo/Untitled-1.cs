1e033daec1f1c30b3ffa336b3ad04c8de15fcced
Bradley Wilson <bradleywilson727@gmail.com>
2022-05-26 10:02:51 -0500

    added steps 6a and 8a

--- Algo/Untitled-1.cs
+++ Algo/Untitled-1.cs
@@ -4,6 +4,8 @@
 4. Place your order.
 5. Pay for your order.
 6. Wait for your delivery.
+6a. Wait even longer for your delivery because they're running behind.
 7. Accept your delivery.
 7a. Argue with driver about how slow they were.
 8. Enjoy your pizza.
+8a. But not too much because you're mad about how late they were.
